e0e419fa82a1802ad53b187b2df6f0d76e9899ee
preritpaliwal <prerit.paliwal@kgpian.iitkgp.ac.in>
2022-09-11 23:08:14 +0530

    data cleaned

--- Asgn1/Q1.py
+++ Asgn1/Q1.py
@@ -1,33 +1,68 @@
-from unicodedata import category
+from re import I
 import pandas as pd
-import numpy as np
-# import matplotlib.pyplot as plt
+import math
 
-df = pd.read_csv("Dataset_A.csv")
+class Node:
+    def __init__(self,attribute):
+        self.attribute = attribute
+        self.children = []
+    
+    def addChild(self,x):
+        self.children.append(x)
 
-"""
-# Removing NAN with Mode
-"""
+def getDFs(df, attr):
+    dfs = []
+    valCnt = df[attr].value_counts()
+    for i in range(len(valCnt)):
+        dfi = df[df[attr]==valCnt.index[i]]
+        dfs.append(dfi)
+    return dfs
 
-naList = df.isna().sum()
-for i in range(len(naList)):
-    if(naList[i]>0):
-        col = naList.index[i]
-        valCnt = df[col].value_counts()
-        df = df.fillna({col:valCnt.index[0]})
+def getEntropy(df):
+    total = len(df["Segmentation"])
+    valCnt = df["Segmentation"].value_counts()
+    ans = 0
+    for i in range(len(valCnt)):
+        tmp = valCnt[i]/total
+        ans -= tmp*math.log(tmp)
+    return ans
 
-# print(df.isna().sum())
+def getIG(df, attr):
+    initialEntropy = getEntropy(df)
+    total = len(df[attr])
+    valCnt = df[attr].value_counts()
+    finalEntropy = 0
+    dfs = getDFs(df,attr)
+    print(valCnt)
+    for i in range(len(dfs)):
+        print(valCnt[i])
+        tmp = valCnt[i]/total
+        finalEntropy += tmp*getEntropy(dfs[i])
+    return initialEntropy - finalEntropy
 
-"""
-LABEL ENCODING
-"""
-df1 = df.dtypes
-for i in range(len(df1)):
-    if df1[i]=="object":
-        col = df1.index[i]
-        df[col] = df[col].astype("category").cat.codes
+def getInfoGainList(df,attrs):
+    infoGain = []
+    for attr in attrs:
+        infoGain.append(getIG(df,attr))
+    return infoGain
 
-print(df.head())
-df = df.drop(columns=["ID"])
-print(df.head())
-df.to_csv("cleanedData.csv",index=False)
+def getNextNode(df):
+    attributes = df.columns.values
+    
+    InfoGainPerAttr = getInfoGainList(df,attributes)
+    
+    i_max = 0
+    for i in range(1,len(attributes)):
+        if InfoGainPerAttr[i]>InfoGainPerAttr[i_max]:
+            i_max = i
+    
+    n = Node(attribute=attributes[i_max])
+    dfs = getDFs(df,attributes[i_max])
+    for df in dfs:
+        n.addChild(getNextNode(df=df))
+    return n
+
+df = pd.read_csv("cleanedData.csv")
+# print(df.head())
+
+decisionTreeClassifier = getNextNode(df)
