5ad8735f644206bb045575e2080de5cc1d1b2a15
Prerit Paliwal <prerit.paliwal@kgpian.iitkgp.ac.in>
2022-11-06 21:20:03 +0530

    Q1 done

--- Asgn2/Q1.py
+++ Asgn2/Q1.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
-from sklearn.preprocessing import StandardScaler
+from data_processing import normalize
 from Kmeans import getInitialRep,Kmeans
 
 df = pd.read_csv("wine.csv")
@@ -11,16 +11,27 @@ x = df.drop(columns=["Target Class"],axis=1)
 y = df["Target Class"]
 
 
-x_normalised = StandardScaler().fit_transform(x)
-
+x_normalised,y_normalised = normalize(x,y)
 
 pca = PCA(0.95)
 principalComponents = pca.fit_transform(x_normalised)
 principalDf = pd.DataFrame(data = principalComponents)
 
-print(pca.explained_variance_ratio_)
-plt.plot(pca.explained_variance_ratio_)
+plt.ylabel("Cumulative Variance")
+plt.xlabel("Number of Principal Components")
+var = pca.explained_variance_ratio_
+for i in range(len(var)-1):
+    var[i+1] = var[i+1]+var[i]
+print("Cumulative Variance based on number of components : ",var)
+plt.plot(var)
 plt.savefig("Variance_VS_Component_number.png", format="png")
+plt.show()
+
+# print("Scatter plot of 1st component before and after PCA: ")
+plt.xlabel("1st Component Before PCA")
+plt.ylabel("1st Component After PCA")
+plt.scatter(principalDf[principalDf.columns[0]],principalDf[principalDf.columns[1]],c=y)
+plt.savefig("PCA_visulation.png", format="png")
 plt.show()
 
 K,NMI = [],[]
@@ -30,8 +41,9 @@ for k in range(2,9):
     K.append(k)
     NMI.append(nmi)
 
-print(K)
-print(NMI)
+print("NMI List with K from [2-8] : ", NMI)
+plt.xlabel("Number of Clusters (k)")
+plt.ylabel("NMI")
 plt.plot(K,NMI)
 plt.savefig("K_VS_NMI.png", format="png")
 plt.show()
